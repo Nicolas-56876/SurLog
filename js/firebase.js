b30cec099c5847d9fb47cb03332c631bbd097eb2
Nicolas-56876 <66097326+Nicolas-56876@users.noreply.github.com>
2026-08-05 21:50:28 -0600

    Add Firestore initialization to firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,14 @@
 // Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 // Configuración de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAim5VOH2DcI_LfhaXFFSJUjIjecRYHhZk",
   authDomain: "surlog-logistics.firebaseapp.com",
-  databaseURL: "https://surlog-logistics-default-rtdb.firebaseio.com",
   projectId: "surlog-logistics",
   storageBucket: "surlog-logistics.firebasestorage.app",
   messagingSenderId: "1029553505961",
@@ -16,8 +18,11 @@ const firebaseConfig = {
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar Authentication
+// Authentication
 const auth = getAuth(app);
 
+// Firestore
+const db = getFirestore(app);
+
 // Exportar
-export { auth };
+export { auth, db };
