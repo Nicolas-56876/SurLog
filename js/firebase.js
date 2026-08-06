791ad220c0883275065a058b3345cb7a960ed4f5
Nicolas-56876 <66097326+Nicolas-56876@users.noreply.github.com>
2026-08-05 20:48:25 -0600

    Update Firebase configuration and initialization
    Updated Firebase configuration with new project details.

--- js/firebase.js
+++ js/firebase.js
@@ -2,20 +2,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-// Configuración
+// Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.firebasestorage.app",
-  messagingSenderId: "XXXXXXXXXX",
-  appId: "XXXXXXXXXXXX"
+  apiKey: "AIzaSyAim5VOH2DcI_LfhaXFFSJUjIjecRYHhZk",
+  authDomain: "surlog-logistics.firebaseapp.com",
+  databaseURL: "https://surlog-logistics-default-rtdb.firebaseio.com",
+  projectId: "surlog-logistics",
+  storageBucket: "surlog-logistics.firebasestorage.app",
+  messagingSenderId: "1029553505961",
+  appId: "1:1029553505961:web:8a5cf9af9313aabc958bb8"
 };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Inicializar Auth
+// Inicializar Authentication
 const auth = getAuth(app);
 
 // Exportar
