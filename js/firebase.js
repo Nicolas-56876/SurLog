1f5ef84f44b8aaa802c6a0c53dbd55de5f62235e
Nicolas-56876 <66097326+Nicolas-56876@users.noreply.github.com>
2026-08-05 23:06:51 -0600

    Add Firebase configuration to firebase.js
    Added Firebase configuration for the project.

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,17 @@
-// Importar Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAim5VOH2DcI_LfhaXFFSJUjIjecRYHhZk",
+  authDomain: "surlog-logistics.firebaseapp.com",
+  databaseURL: "https://surlog-logistics-default-rtdb.firebaseio.com",
+  projectId: "surlog-logistics",
+  storageBucket: "surlog-logistics.firebasestorage.app",
+  messagingSenderId: "1029553505961",
+  appId: "1:1029553505961:web:8a5cf9af9313aabc958bb8",
+  measurementId: "G-FCY3Y8YKY9"
+};
 
 const app = initializeApp(firebaseConfig);
 
