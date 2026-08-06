9e512ca7b8fb0292602a20eebf12031dd5e33c9b
Nicolas-56876 <66097326+Nicolas-56876@users.noreply.github.com>
2026-08-05 21:59:40 -0600

    Remove Firebase configuration from firebase.js
    Removed Firebase configuration details from the file.

--- js/firebase.js
+++ js/firebase.js
@@ -5,24 +5,9 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth
 
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyAim5VOH2DcI_LfhaXFFSJUjIjecRYHhZk",
-  authDomain: "surlog-logistics.firebaseapp.com",
-  projectId: "surlog-logistics",
-  storageBucket: "surlog-logistics.firebasestorage.app",
-  messagingSenderId: "1029553505961",
-  appId: "1:1029553505961:web:8a5cf9af9313aabc958bb8"
-};
-
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Authentication
 const auth = getAuth(app);
-
-// Firestore
 const db = getFirestore(app);
 
-// Exportar
 export { auth, db };
